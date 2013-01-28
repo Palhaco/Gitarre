8b15773ccb18fe12fb30dfa64f4e105e7aa6cef5
Fred <Fred.Brockstedt@BeLUG.de>
2013-01-28 22:29:41 +0100

    kleines Testprogramm fuer die Tonausgabe

---
+++ Play.java
@@ -0,0 +1,24 @@
+class Play {
+    public static void main(String[] args) {
+	double freq = 329;
+	for (int i = 0; i < 44100; i++) {
+	    StdAudio.play(0.5 * Math.sin(2*Math.PI * freq * i / 44100));
+	    System.out.println(0.5 * Math.sin(2*Math.PI * freq * i / 44100));
+	}
+	while(true) {
+	    /*
+	    for(double i=0; i<=6.28; i+=0.05) {
+		StdAudio.play(2 * Math.sin(i));
+		System.out.print("\r"+i);
+	    } 
+	    */
+	    /*
+	    for(double i=0; i<=.05; i+=0.01) {
+		StdAudio.play(.629 );
+	    }
+	    */
+
+
+	}
+    }
+}
