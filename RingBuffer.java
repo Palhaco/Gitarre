9c0219c62c9a413b0afbe6400c3d2b67e49a9a22
Fred <Fred.Brockstedt@BeLUG.de>
2013-01-28 21:36:04 +0100

    RingBuffer fix fuer index last und first siehe eMail vom 28.Januar

---
+++ RingBuffer.java
@@ -0,0 +1,88 @@
+public class RingBuffer {
+	
+    int maxCapacity = 0;
+    int first = 0;
+    int last = 0;
+    int size = 0;
+	
+    double[] buffer;
+	
+    /**
+     * constructor to create an empty buffer, with given max capacity
+     * @param capacity
+     */
+    RingBuffer(int capacity)
+    {
+	maxCapacity = capacity;
+	buffer = new double[maxCapacity];
+    }  
+	  
+    /**
+     * return number of items currently in the buffer
+     * @return
+     */
+    public int size()  
+    {
+	return size;
+    }   
+             
+    /**
+     * is the buffer empty (size equals zero)?
+     * @return
+     */
+    public boolean isEmpty()
+    {
+	return size == 0;
+    }    
+	  
+    /**
+     * is the buffer full  (size equals capacity)?
+     * @return
+     */
+    public boolean isFull()   
+    {
+	return maxCapacity == size;
+    } 
+	  
+    /**
+     * add item x to the end
+     * @param x
+     */
+    public void enqueue(double x) 
+    {
+	if(isFull())
+	    return;
+		  
+	size++;
+	last = last++ % maxCapacity;
+		  
+	buffer[last] = x;
+    } 
+	  
+    /**
+     * delete and return item from the front
+     * @return
+     */
+    public double dequeue()
+    {
+	if(isEmpty())
+	    return Double.NaN;
+		  
+	size--;
+	first = first++ % maxCapacity;
+		  
+	return buffer[first];
+    }
+	  
+    /**
+     * return (but do not delete) item from the front
+     * @return
+     */
+    public double peek() 
+    {
+	if(isEmpty())
+	    return Double.NaN;
+		  
+	return buffer[first];  
+    } 
+}
